4ddf3ac03cf1e53452a9f55fcafad0825d0e0b57
RachidLaasri <rashidlaasri@gmail.com>
2015-11-25 22:35:17 +0000

    Fix DocBlock

--- src/helpers.php
+++ src/helpers.php
@@ -7,9 +7,9 @@ if ( ! function_exists('t'))
     /**
      * Get a string from language file.
      *
-     * @param $string
-     * @param null $default
-     * @param array $parameters
+     * @param string  $string
+     * @param string  $default
+     * @param array   $parameters
      * @return mixed
      */
     function t($string, $default = null, $parameters = [])
